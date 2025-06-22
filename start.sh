3bac3ea61631bc1ae717610c9efe936e9e9b15bd
evonp <ttnf91801@gmail.com>
2025-06-22 15:45:49 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 安装系统依赖
+# 安装浏览器依赖
 apt-get update
-apt-get install -y curl gpg libnss3 libnspr4 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxkbcommon0 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libasound2
+apt-get install -y libnss3 libnspr4 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxkbcommon0 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libasound2
 
 # 设置 WARP 代理
 python3 -c "from proxy_manager import setup_warp_proxy; setup_warp_proxy()"
